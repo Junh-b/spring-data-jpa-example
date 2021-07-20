96c53be55182ca512f463f04203a8700965658ca
Baek Junha <jooha208@gmail.com>
2021-07-20 21:20:57 +0900

    initial code

---
+++ src/main/java/net/junhabaek/spring_data_jpa_example/SpringDataJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package net.junhabaek.spring_data_jpa_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/junhabaek/spring_data_jpa_example/SpringDataJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.junhabaek.spring_data_jpa_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
